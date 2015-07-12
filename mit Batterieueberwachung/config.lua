db1f43b1def00f8e6c801e1c8bf2d06147dc83d5
8n1 <zollfrei@hotmail.com>
2015-07-12 05:01:25 +0200

    config angepasst

--- mit Batterieueberwachung/config.lua
+++ mit Batterieueberwachung/config.lua
@@ -1,9 +1,9 @@
 
 use_battery_check = true
 use_temp_sensor = true
-use_ipcam_event = false
+use_ipcam_event = true
 
-use_single_devid = true
+use_single_devid = false
 
 --------------------------------------
 -- WIFI configuration
@@ -13,7 +13,7 @@ wifi.sta.connect()
 
 --------------------------------------
 -- Pushingbox device id
-devid = "v666E3BC8B6E6690"
+devid = "xxxxx"
 
 --------------------------------------
 -- Voltage divider values and vref
@@ -24,28 +24,28 @@ r2 = 10000
 bat_info = "OK"
 -- Warning voltage 1 & 2 (2 must be lower then 1)
 warn_volt1 = 3.7    -- e.g battery half empty
-warn_devid1 = "vA0B298E993F6878"
+warn_devid1 = "xxxxx"
 warn_info1 = "50%"
 
-warn_volt2 = 3.4    -- e.g battery almost dead
-warn_devid2 = "v1F4F5FC530AB030"
+warn_volt2 = 3.3    -- e.g battery almost dead
+warn_devid2 = "xxxxx"
 warn_info2 = "20%"
 
 --------------------------------------
 -- Temperature sensor pin (nodemcu I/O Index)
-tempsensor_pin = 2
+tempsensor_pin = 4
 -- Decimal places for sensor value
 precision = 1
 
 --------------------------------------
 -- Server to get the time from (not NTP)
-time_server_ip = "192.168.0.123"
+time_server_ip = "192.168.1.123"
 -- Time offset
-time_offset = 2
+time_offset = 1
 
 --------------------------------------
 -- IP cam config
-cam_ip = "192.168.0.123"
+cam_ip = "192.168.1.18"
 cam_port = 8001
 event_url = "/axis-cgi/io/virtualinput.cgi?action=6:/5000"
 base64_pass = "am9oOmpvaA=="
