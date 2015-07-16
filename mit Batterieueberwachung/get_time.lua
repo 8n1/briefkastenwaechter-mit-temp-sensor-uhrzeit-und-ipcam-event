69c039d32f642a43f45144d742da43581f70e932
8n1 <zollfrei@hotmail.com>
2015-07-16 17:06:37 +0200

    update

--- mit Batterieueberwachung/get_time.lua
+++ mit Batterieueberwachung/get_time.lua
@@ -1,42 +1,76 @@
--- Get time from webserver by parsing the HTTP requests "Date" header
--- see http://benlo.com/esp8266/esp8266Projects.html (-> Ask Google for the Time)
 
-conn = net.createConnection(net.TCP, 0)        
-conn:on("receive", function(conn, payload)
-    if string.find(payload, "HTTP/1.1 200 OK") then
-        res = string.sub(payload, string.find(payload, "Date: ") + 6, string.find(payload, "Date: ") + 30)
+if use_date_time == true then
 
-        date = string.sub(res, 0, string.find(res, ":") - 3)
-        time = string.sub(res, string.find(res, ":") - 2, string.find(res, ":") + 5)
-        
-        hours = string.sub(time, 0, 2)
-        min_secs = string.sub(time, 3, 8)
-        calc_hours = (hours+time_offset) % 24
-        if calc_hours < 10 then
-            calc_hours = "0" ..calc_hours
-        end
-        time = calc_hours..min_secs
-        
-        print(" -> Date: " ..date)
-        print(" -> Time: " ..time)
-        print("")
-        
-        res, hours, min_secs, calc_hours = nil
-        collectgarbage()
-        
+    print(" Getting time...")
+
+    -- do a HEAD request and extract date and time from the response headers 'Date' field
+    conn = net.createConnection(net.TCP, 0)        
+    conn:on("receive", function(conn, payload)
+        -- if got response...
+        if string.find(payload, "HTTP/1.1 200 OK") then
+
+            -- get the 'Date' header field (Example: 'Date: Tue, 15 Nov 1994 08:12:31 GMT')
+            res = string.sub(payload, string.find(payload, "Date: ") + 6, string.find(payload, "Date: ") + 30)
+
+            -- extract date
+            date = string.sub(res, 0, string.find(res, ":") - 3)
+
+            -- TODO: date foo
+
+            -- extract time
+            time = string.sub(res, string.find(res, ":") - 2, string.find(res, ":") + 5)
+            
+            -- extract hours, minutes and seconds
+            hours = string.sub(time, 0, 2)
+            minutes = string.sub(time, 4, 5)
+            seconds = string.sub(time, 7, 8)
+
+            -- time correction hack (add time_offset and handle a potential overflow)
+            hours = (hours+time_offset) % 24
+
+            -- re-add the leading zero which might just got "trunculated"
+            hours = string.format("%02d", hours)
+
+            -- time format: 00:00:00
+            time = hours..":"..minutes
+            -- time format: 00:00
+            time_n = hours..":"..minutes..":"..seconds
+            
+            -- print date and time
+            print(" -> Date: " ..date)
+            print(" -> Time: " ..time..":"..seconds)
+            print("")
+            
+            -- collect some garbage (NOTE: test if still neccessary)
+            collectgarbage()
+            
+            -- make the Pushingbox Scenario request
+            print(" Triggering Scenario " ..send_warning.. " ("..bat_info..")...")
+            if DEBUG ~= true then
+                dofile("trigger_scenario.lc")
+            end
+
+       else
+          print(payload)
+          print(" -> FAIL\n")
+       end
+
+    end)
+    conn:connect(80, time_server_ip)
+    conn:send("HEAD / HTTP/1.1\r\n"..
+       "Host: " ..time_server_ip.. "\r\n"..
+       "Connection: close\r\n"..
+       "Accept: */*\r\n"..
+       "\r\n")
+    conn = nil
+
+else
+    date = "0"
+    time = "0"
+    seconds = "0"
+
+    print(" Triggering Scenario " ..send_warning.. " ("..bat_info..")...")
+    if DEBUG ~= true then
         dofile("trigger_scenario.lc")
-        print(" Triggering Scenario " ..send_warning.. " ("..bat_info..")...")
-        triggerPushingboxScenario(date, time)
-
-   else
-      print(payload)
-      print(" -> FAIL\n")
-   end
-end)
-conn:connect(80, time_server_ip)
-conn:send("HEAD / HTTP/1.1\r\n"..
-   "Host: " ..time_server_ip.. "\r\n"..
-   "Connection: close\r\n"..
-   "Accept: */*\r\n"..
-   "\r\n")
-conn = nil
+    end
+end
--- mit Batterieueberwachung/init.lua
+++ mit Batterieueberwachung/init.lua
@@ -1,5 +1,16 @@
+
+-- Set to Station Mode
+wifi.setmode(wifi.STATION)
+
 -- Load user config
 dofile("config.lua")
+
+if use_static_ip then
+    wifi.sta.setip({ip=sensor_ip, netmask=sensor_netmask, gateway=sensor_gateway})
+end
+
+-- Connect to AP
+wifi.sta.connect()
 
 -- Small intro
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -8,61 +19,84 @@ print(" mit Temperatur, Uhrzeit,          ")
 print(" IP Cam und Batterie-ueberwachung  ")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
--- If just want to use a single devid
-if use_single_devid == true then
+-- Only use a single devid?
+if use_single_devid then
     print("Single devid")
     warn_devid1 = devid
     warn_devid2 = devid
 end
 
-print("")
+-- Startup time 
+startup_time = dhcp_startup_time
+if use_static_ip then
+    print("Static IP\n")
+    startup_time = static_startup_time
+else
+    print("DHCP\n")
+end
 
--- Check battery voltage and set 'send_warning', 'devid' and 'bat_info'
+-- Get Wifi strength
+rssi = 0
+quality = 0
+if use_wifi_strength then
+    dofile("get_rssi.lc")
+end
+
+-- Get battery voltage
 vin = 0
 send_warning = 0
-if use_battery_check == true then
-    dofile("check_volts.lc")
+--bat_info
+if use_battery_check then
+    dofile("get_battery_voltage.lc")
     vin = check_volts()
 
-    -- Print some informations
     print("Batterie Spannung: "..string.format("%.2f", vin).."V")
     print("Warn Modus: "..send_warning)
     print("Bat: "..bat_info.."\n")
 
     check_volts = nil
 end
-
+    
 -- Wait until we have an IP from the AP
-tmr.alarm(0, 1000, 1, function()
+tmr.alarm(0, startup_time, 1, function()
     print(" Checking IP...")
     if wifi.sta.getip() == nil then
+        tmr_now = tmr.now()
         -- Do nothing
     else
-        dhcp_time = string.format("%.2f", tmr.now()/1000/1000)
         tmr.stop(0)
-        print(" -> IP: " ..wifi.sta.getip().. "\n")
 
-        -- Get ds temperature
-        if use_temp_sensor == true then
+        -- Time to get an IP
+        ip_time = string.format("%.2f", tmr.now()/1000/1000)
+
+        -- Print ip and wifi strength quality
+        print(" -> IP: " ..wifi.sta.getip())
+        if use_wifi_strength then
+            print(" -> RSSI is: "..rssi.."dBm")
+            print(" -> Quality is: "..quality.."%\n")
+        else
+            print("")
+        end
+
+        -- Get temperature
+        ds_temp = 0
+        if use_temp_sensor then
             print(" Getting temperature...  ")
-            dofile("ds_init.lc")
+            dofile("get_ds_temp.lc")
             debug = 1
-            dofile("ds_init.lc")
+            dofile("get_ds_temp.lc")
             print(" -> Temperature: " ..ds_temp.. "'C")
             print("")
-        else
-            ds_temp = 0
         end
 
         -- Activate ip cam event
-        if use_ipcam_event == true then
+        if use_ipcam_event then
             print(" Triggering IP Cam Event...")
             dofile("trigger_ipcam_event.lc")
             print("")
         end
         
-        -- Get time and trigger Pushingbox notification
-        print(" Getting time...")
+        -- Get time and activate Pushingbox Scenario
         dofile("get_time.lc")
     end
 end)
@@ -72,5 +106,7 @@ tmr.alarm(1, timout*1000, 0, function()
     print("\n~~~~~~~~~~~~~~~~~~~~~~~")
     print(" Forcing DeepSleep...    ")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-    node.dsleep(0, 1)
+    if DEBUG ~= true then
+        node.dsleep(0, 1)
+    end
 end)
