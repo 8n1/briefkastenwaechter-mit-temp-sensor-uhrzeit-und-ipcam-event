f8f16db081965ec66d563773bb1d5d8e54cc0890
8n1 <zollfrei@hotmail.com>
2015-08-09 19:46:14 +0200

    update - 0.9.5

---
+++ 0.9.5/deepsleep.lua
@@ -0,0 +1,13 @@
+-- Activate cyclic or infinite DeepSleep
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(" Heap: " ..node.heap()/1000 .." KB free")
+print(" Timer: " ..string.format("%.2f", tmr.now()/1000/1000) .." seconds elapsed\n")
+
+if USE_CYCLIC_DEEPSLEEP then
+    print(" Waking up in " ..sleep_time .." minutes.")
+    node.dsleep(sleep_time*60*1000*1000 - tmr.now(), 1)
+else
+    -- infinite deep sleep
+    node.dsleep(0, 1)
+end
